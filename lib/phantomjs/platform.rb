32052b25e2e311d847bdcdb196acebe0473ff140
Denis <endenis@gmail.com>
2015-04-29 14:53:16 +0200

    Switched to self-hosted binary for mac os x

--- lib/phantomjs/platform.rb
+++ lib/phantomjs/platform.rb
@@ -130,7 +130,7 @@ module Phantomjs
         end
 
         def package_url
-          'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-macosx.zip'
+          'https://dl.dropboxusercontent.com/u/8133370/important/phantomjs-2.0.0-macosx.zip'
         end
       end
     end
